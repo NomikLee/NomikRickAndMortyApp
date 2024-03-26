9cfa49e4b9b1cedf1d67e7d7c5766046ce0919e7
NomikLee <Nomiklee.icloud.com>
2024-03-26 16:21:45 +0800

    Added readme and gitignore

---
+++ README.md
@@ -0,0 +1,4 @@
+# Swift Rick & Morty App
+
+Full featured universal (iOS, macOS, iPadOS) app showcasing the Rick and Morty API.
+
--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -11,7 +11,7 @@ class RMCharacterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .systemGreen
         title = "Characters"
         
     }
